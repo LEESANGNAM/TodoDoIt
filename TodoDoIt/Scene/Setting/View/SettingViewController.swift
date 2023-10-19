9813f04c4269d4a1192fd2991929bdd7362791f0
LEESANGNAM <fgh0116@naver.com>
2023-10-20 03:33:00 +0900

    [Feat] 데이터 초기화 추가 초기화, 복구 시 alert 띄우기

--- TodoDoIt/Scene/Setting/View/SettingViewController.swift
+++ TodoDoIt/Scene/Setting/View/SettingViewController.swift
@@ -31,6 +31,7 @@ class SettingViewController: BaseViewController {
     }
 }
 
+//MARK: - tableViewDelegate
 extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
         return settingList.count
@@ -52,7 +53,6 @@ extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
         cell.backgroundColor = Design.Color.background
         let data = settingDataList[indexPath.section][indexPath.row]
         cell.textLabel?.text = data
-        
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -63,7 +63,9 @@ extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
             let dataSecion = dataEnumList[indexPath.row]
             switch dataSecion {
             case .reset:
-                break
+                showAlert(text: "기존 데이터가 전부 삭제됩니다. \n 정말 삭제하시겠습니까?",addButtonText: "확인") {[weak self] in
+                    self?.resetData()
+                }
             case .backup:
                 let today = Date().changeFormatString(format: "yyyyMMdd")
                 let fileName = "TodoDoIt-\(today)"
@@ -71,91 +73,19 @@ extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
 
                 showActivityViewController(fileURL: zipfileURL)
             case .restore:
-                let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [.archive], asCopy: true)
-                documentPicker.delegate = self
-                documentPicker.allowsMultipleSelection = false // 여러개 선택 막기
-                
-                present(documentPicker, animated: true)
+                showAlert(text: "기존 데이터가 전부 삭제됩니다. \n 백업 데이터로 복구하시겠습니까?",addButtonText: "복구") {[weak self] in
+                    self?.showDocumentViewController()
+                }
             }
         case .opneSource:
             break
         }
         
-
+        tableView.deselectRow(at: indexPath, animated: true)
     }
     
 }
-extension SettingViewController: UIDocumentPickerDelegate {
-    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
-        print(#function)
-    }
-    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
-        guard let selectedFileURL = urls.first else { // 파일 앱에서 선택한 url
-            print("선택한 파일에오류가 있어요~")
-            return
-        }
-        guard let path = FileManager.getDocumentDirectoryPath() else{
-            print("도큐먼트 위치에 오류가 있어요")
-            return
-        }
-        print(selectedFileURL, "선택한 파일 검사")
-        
-        guard isValidBackupFile(fileURL: selectedFileURL) else {
-            print("잘못된 zip 파일")
-            return
-        }
-        
-        // 도큐먼트 폴더 내 저장할 경로 설정
-        let sandboxFileURL = path.appendingPathComponent(selectedFileURL.lastPathComponent)
-        //경로에 복구할 파일 (zip)이 이미 있는지 확인
-        if FileManager.default.fileExists(atPath: sandboxFileURL.path){
-//            let fileURL = path.appendingPathComponent("SangnamArchive.zip")
-            
-            do {                  // 어떤파일, 어디다가풀건가, 덮어쓰기, 패스워드, 압축 진행률
-                try Zip.unzipFile(sandboxFileURL, destination: path, overwrite: true, password: nil,progress: { progress in
-                    print("============progress: ",progress)
-                },fileOutputHandler: { unzippedFile in
-                    print("===========unzippedfile(압축해제 완료)",unzippedFile)
-                    print("압축 종료됨이건 클로저 안쪽 호출")
-                })
-                dismissToast()
-            }catch{
-                print("압축해제실패")
-            }
-            
-        }else {
-            do {
-                try FileManager.default.copyItem(at: selectedFileURL, to: sandboxFileURL)
-                try Zip.unzipFile(sandboxFileURL, destination: path, overwrite: true, password: nil,progress: { progress in
-                    print("============progress: ",progress)
-                },fileOutputHandler: { unzippedFile in
-                    print("===========unzippedfile(압축해제 완료)",unzippedFile)
-                    print("압축 종료됨이건 클로저 안쪽 호출")
-                })
-                dismissToast()
-            } catch{
-                print("여기뜨는거임?")
-                print("압축해제실패 오류~")
-            }
-            //경로에 복구할 파일이 없을 때의 대응
-        }
-        
-    }
-    private func dismissToast(){
-        self.view.makeToast("잠시뒤 앱이 종료됩니다. 다시 켜주세요")
-        showAlert(text: "앱을 재실행해야 복구 데이터가 적용 됩니다. 앱을 재실행 해주세요",addButtonText: "확인") {
-            exit(2)
-        }
-    }
-    private func isValidBackupFile(fileURL: URL) -> Bool {
-        let appName = "TodoDoIt"
-        // 마지막 : 파일명 0000.zip
-        let fileName = fileURL.lastPathComponent
-        // 앱 이름이 포함되어 있는지 확인
-        return fileName.contains(appName)
-    }
-}
-
+//MARK: didselect Event
 extension SettingViewController {
     // Document 폴더에 "Images" 디렉토리와 "default.realm" 파일을 압축하여 저장하는 함수
     func backupDataAndImagesToDocumentDirectory(fileNamge: String) -> URL? {
@@ -195,7 +125,7 @@ extension SettingViewController {
             }
         }
     
-    func showActivityViewController(fileURL: URL){
+    private func showActivityViewController(fileURL: URL){
         guard let path = FileManager.getDocumentDirectoryPath() else {
             print("도큐먼트 오류")
             return
@@ -203,6 +133,113 @@ extension SettingViewController {
         let vc = UIActivityViewController(activityItems: [fileURL], applicationActivities: [])
         present(vc,animated: true)
     }
+    private func showDocumentViewController(){
+        let documentPicker = UIDocumentPickerViewController(forOpeningContentTypes: [.archive], asCopy: true)
+        documentPicker.delegate = self
+        documentPicker.allowsMultipleSelection = false // 여러개 선택 막기
+        
+        present(documentPicker, animated: true)
+    }
+    
+    private func resetData() {
+        // Realm 데이터 초기화
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.deleteAll()
+            }
+            print("Realm 데이터 초기화 성공")
+        } catch {
+            print("Realm 데이터 초기화 실패: \(error.localizedDescription)")
+            return
+        }
+
+        // Images 디렉토리 삭제
+        guard let imagesDirectory = FileManager.getDocumentDirectoryPath()?.appendingPathComponent("Images") else {
+            print("Images 디렉토리를 찾을 수 없습니다.")
+            return
+        }
+
+        do {
+            try FileManager.default.removeItem(at: imagesDirectory)
+            print("Images 디렉토리 삭제 성공")
+        } catch {
+            print("Images 디렉토리 삭제 실패: \(error.localizedDescription)")
+        }
+    }
+}
+
+
+//MARK: - DocumentPickerDelegate: 파일가져오기
+extension SettingViewController: UIDocumentPickerDelegate {
+    func documentPickerWasCancelled(_ controller: UIDocumentPickerViewController) {
+        print(#function)
+    }
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        guard let selectedFileURL = urls.first else { // 파일 앱에서 선택한 url
+            print("선택한 파일에오류가 있어요~")
+            return
+        }
+        guard let path = FileManager.getDocumentDirectoryPath() else{
+            print("도큐먼트 위치에 오류가 있어요")
+            return
+        }
+        print(selectedFileURL, "선택한 파일 검사")
+        
+        guard isValidBackupFile(fileURL: selectedFileURL) else {
+            print("잘못된 zip 파일")
+            return
+        }
+        
+        // 도큐먼트 폴더 내 저장할 경로 설정
+        let sandboxFileURL = path.appendingPathComponent(selectedFileURL.lastPathComponent)
+        //경로에 복구할 파일 (zip)이 이미 있는지 확인
+        if FileManager.default.fileExists(atPath: sandboxFileURL.path){
+            do {                  // 어떤파일, 어디다가풀건가, 덮어쓰기, 패스워드, 압축 진행률
+                try Zip.unzipFile(sandboxFileURL, destination: path, overwrite: true, password: nil,progress: { progress in
+                    print("============progress: ",progress)
+                },fileOutputHandler: { unzippedFile in
+                    print("===========unzippedfile(압축해제 완료)",unzippedFile)
+                    print("압축 종료됨이건 클로저 안쪽 호출")
+                })
+                dismissToast()
+            }catch{
+                print("압축해제실패")
+            }
+            
+        }else {
+            do {
+                try FileManager.default.copyItem(at: selectedFileURL, to: sandboxFileURL)
+                try Zip.unzipFile(sandboxFileURL, destination: path, overwrite: true, password: nil,progress: { progress in
+                    print("============progress: ",progress)
+                },fileOutputHandler: { unzippedFile in
+                    print("===========unzippedfile(압축해제 완료)",unzippedFile)
+                    print("압축 종료됨이건 클로저 안쪽 호출")
+                })
+                dismissToast()
+            } catch{
+                print("여기뜨는거임?")
+                print("압축해제실패 오류~")
+            }
+            //경로에 복구할 파일이 없을 때의 대응
+        }
+        
+    }
+    private func dismissToast(){
+        self.view.makeToast("잠시뒤 앱이 종료됩니다. 다시 켜주세요")
+        showAlert(text: "앱을 재실행해야 복구 데이터가 적용 됩니다. 앱을 재실행 해주세요",addButtonText: "확인") {[weak self] in
+            exit(2)
+        }
+    }
+    // 앱 이름 들어갔는지 복구파일 검사
+    private func isValidBackupFile(fileURL: URL) -> Bool {
+        let appName = "TodoDoIt"
+        // 마지막 : 파일명 0000.zip
+        let fileName = fileURL.lastPathComponent
+        // 앱 이름이 포함되어 있는지 확인
+        return fileName.contains(appName)
+    }
+
 }
 
 
