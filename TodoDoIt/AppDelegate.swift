ca779427d1f62aa2ae95a76905717f89d26060b4
LEESANGNAM <fgh0116@naver.com>
2023-10-27 04:58:38 +0900

    [Feat] local notification 오후 8시 할일 목록 검사후 알림등록

--- TodoDoIt/AppDelegate.swift
+++ TodoDoIt/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseCore
+import FirebaseAnalytics
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,43 +17,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        Analytics.logEvent(AnalyticsEventAppOpen, parameters: nil)
         UNUserNotificationCenter.current().delegate = self
-        
+        // 이전 등록 알림 삭제
+        UNUserNotificationCenter.current().removePendingNotificationRequests(withIdentifiers: ["customNotification"])
         // 알림 권한 설정
         UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge]) { success, error in
             if success{
-                self.dailyNineTimeNotification()
                 print("성공한건가")
             }else{
                 print(error)
             }
         }
+
         return true
     }
-    
-    func dailyNineTimeNotification(){
-        // 포그라운드에서 알림이 안뜨는게 디폴트
-        // 알림보내!
-        //1. 컨텐츠 2. 언제
-        
-        //1. 컨텐츠 채우기
-        let content = UNMutableNotificationContent()
-        content.title = "테스트 타이틀"
-        content.body = "테스트 바디"
-        
-        var component = DateComponents() //구조체라 var
-        component.minute = 0 // 5분
-        component.hour = 9
-        
-        let trigger = UNCalendarNotificationTrigger(dateMatching: component, repeats: true)
-        
-        //2. 언제
-        let request = UNNotificationRequest(identifier: "dailyNineTimeNotification", content: content, trigger: trigger)
-        
-        UNUserNotificationCenter.current().add(request) { error in
-            print(error)
-        }
-        
+   
         // MARK: UISceneSession Lifecycle
         
         func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -66,10 +46,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
             // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
         }
-        
-        
     }
-}
 extension AppDelegate: UNUserNotificationCenterDelegate {
     func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
         // 특정화면, 특정조건에서만 포그라운드 알림 받기,
--- TodoDoIt/SceneDelegate.swift
+++ TodoDoIt/SceneDelegate.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
@@ -49,8 +49,53 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+        // 백그라운드로 갈때 알림 푸쉬
+        checkTodosAndPerformAction()
     }
-
-
+    
+    // 할일 검사후 알림
+    func checkTodosAndPerformAction(){
+        UNUserNotificationCenter.current().getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                // 할일 정보 가져오기
+                let todoRepository = TodoRepository()
+                let todos = todoRepository.fetchFilterDateSortByFinish(date: Date())
+                
+                // 끝나지 않은 할일이 있다면 알림 설정
+                if !todos.isEmpty,
+                   let firstTodo = todos.first, !firstTodo.finish {
+                    self.sendNotification(title: "할일 알림", body: "아직 남은 할일이 있어요. 확인해보세요!")
+                } else {
+                    // 할일이 없으면 다른 메시지 보내기
+                    self.sendNotification(title: "할일 알림", body: "오늘 하루도 수고하셨어요. 내일 할일을 설정해보세요!")
+                }
+            } else {
+                print("권한없음")
+            }
+        }
+    }
+    
+    // 알림 설정
+    func sendNotification(title: String, body: String) {
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.body = body
+        
+        // 시간
+        var dateComponents = DateComponents()
+        dateComponents.hour = 20
+        dateComponents.minute = 0
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        // identifier
+        let request = UNNotificationRequest(identifier: "customNotification", content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("알림 보내기 실패: \(error)")
+            } else {
+                print("알림 보내기 성공")
+            }
+        }
+    }
 }
-
